1edd7eabc19bd3b40a508b0f0be9e4b2402582a5
hamlim <hamlim@rpi.edu>
2016-02-20 02:37:30 -0500

    some changes

--- assets/js/music.js
+++ assets/js/music.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
     //we want to fetch the tracks
+    var musicContainer = document.getElementById('music-container');
+
     function checkStatus(response) {
         if (response.status >= 200 && response.status < 300) {
-            return response
+            return response;
         } else {
             var error = new Error(response.statusText)
             error.response = response
@@ -11,7 +13,7 @@ $(document).ready(function(){
     }
 
     function parseJSON(response) {
-        return response.json()
+        return response.json();
     }
     // <div class='row'><div class='one column track__number'><span id='track__number'></span></div><div class='three columns track__name'><span id='track__name'></span></div><div class='two columns track__album'><span id='track__album'></span></div><div class='six columns track__audio'><audio id='track__audio'></audio></div></div>
     fetch('https://api.airtable.com/v0/app35atgWtDtBboot/music', {
@@ -23,7 +25,20 @@ $(document).ready(function(){
     }).then(checkStatus)
     .then(parseJSON)
     .then(function(response){
-        console.log(response)
+        console.log(response);
+        //lets get that data
+        for(var i=0; i<response.records.length; i++){
+            console.log("For Loop");
+            var object = response.records[i];
+            console.log(object);
+            console.log(object.fields.song_title);
+            if(object.fields.song_title !== undefined){
+                console.log("For Loop conditional");
+                var fields = object.fields;
+                var template = "<div class='row'><div class='one column track__number'><span id='track__number'>"+ fields.song_id +"</span></div><div class='three columns track__name'><span id='track__name'>"+ fields.song_title +"</span></div><div class='four columns track__album'><span id='track__album'>"+ fields.album_name +"</span></div><div class='four columns track__audio'><audio id='track__audio' src='"+ fields.song_file[0].url+"' controls></audio></div></div>";
+                musicContainer.innerHTML += template;
+            }
+        }
     }, function(error){
         console.log(error);
     });
